abca97cd83ba7ef5bbe84e8cab2cbfae6060cf50
Thomas Porro <44237521+thomasporro@users.noreply.github.com>
2024-12-29 16:25:05 +0100

    feat: added docs and asserts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,13 @@
+## 0.0.3
+
+* Add docs to AnimatedBorderPainter
+
+* Add assert to AnimatedBorderPainter to ensure the validity of the parameters
+
 ## 0.0.2
 
 * Update pubspec.yaml and README.md
 
 ## 0.0.1
 
-* Initial release 
+* Initial release
--- lib/src/animated_border_painter.dart
+++ lib/src/animated_border_painter.dart
@@ -2,19 +2,66 @@ import 'dart:math';
 
 import 'package:flutter/widgets.dart';
 
+/// A custom painter used to draw a rotating gradient border around a
+/// rectangular container.
 class AnimatedBorderPainter extends CustomPainter {
+  /// The constructor for the [AnimatedBorderPainter] class.
+  ///
+  /// Inside it uses the provided [angle], [radius], [thickness], [glowRadius],
+  /// and [gradientColors] to draw a rotating gradient border around a
+  /// rectangular container. In the specific it uses [SweepGradient] to create a
+  /// gradient along the border of the container, and uses [MaskFilter.blur] to
+  /// apply a blur effect to the gradient to create a glowing effect.
   AnimatedBorderPainter({
     required this.angle,
     required this.gradientColors,
     this.radius = 0,
     this.thickness = 1,
     this.glowRadius = 2.0,
-  });
+  })  : assert(
+          gradientColors.length > 1,
+          'AnimatedBorderPainter requires at least two colors to create a gradient',
+        ),
+        assert(
+          glowRadius >= 0,
+          'AnimatedBorderPainter glowRadius must be greater than or equal to 0',
+        ),
+        assert(
+          radius >= 0,
+          'AnimatedBorderPainter radius must be greater than or equal to 0',
+        ),
+        assert(
+          thickness >= 0,
+          'AnimatedBorderPainter thickness must be greater than or equal to 0',
+        );
 
+  /// The radius of the border.
+  ///
+  /// Defaults to 0.
   final double radius;
+
+  /// The thickness of the border.
+  ///
+  /// Defaults to 1.
   final double thickness;
+
+  /// The angle of the gradient.
+  ///
+  /// Is it uses to determine the position of the colors along the border and
+  /// the glowing effect.
   final double angle;
+
+  /// The radius of the glowing effect.
+  ///
+  /// Defaults to 2.
   final double glowRadius;
+
+  /// The colors of the gradient.
+  ///
+  /// The length of the list should be at least 2.
+  ///
+  /// The gradient is created using [SweepGradient] and is rotated by the
+  /// provided [angle] to create a rotating gradient.
   final List<Color> gradientColors;
 
   @override
