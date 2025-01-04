701265c6117ad9fc172da8bb4489771532e7cf8c
Thomas Porro <44237521+thomasporro@users.noreply.github.com>
2025-01-04 22:21:45 +0100

    feat: add margin support

--- lib/src/animated_border_painter.dart
+++ lib/src/animated_border_painter.dart
@@ -15,9 +15,11 @@ class AnimatedBorderPainter extends CustomPainter {
   AnimatedBorderPainter({
     required this.angle,
     required this.gradientColors,
+    this.margin,
     this.radius = 0,
     this.thickness = 1,
     this.glowRadius = 2.0,
+    this.textDirection = TextDirection.ltr,
   })  : assert(
           gradientColors.length > 1,
           'AnimatedBorderPainter requires at least two colors to create a gradient',
@@ -64,13 +66,71 @@ class AnimatedBorderPainter extends CustomPainter {
   /// provided [angle] to create a rotating gradient.
   final List<Color> gradientColors;
 
+  /// The margin of the container.
+  ///
+  /// This variable MUST be of type [EdgeInsets] or
+  /// [EdgeInsetsDirectional] in order to be used. In this way is possibile to
+  /// add correctly the margin of a container.
+  ///
+  /// This will modify where the gradient is drawn
+  final EdgeInsetsGeometry? margin;
+
+  /// The text direction used in the app.
+  ///
+  /// Is used to resolve the correct direction of the margin in the glowing
+  /// effect.
+  final TextDirection textDirection;
+
   @override
   void paint(final Canvas canvas, final Size size) {
+    late ({
+      double left,
+      double top,
+      double right,
+      double bottom,
+    }) marginRecord;
+
+    switch (margin?.runtimeType) {
+      case == EdgeInsets:
+        final EdgeInsets localMargin = margin! as EdgeInsets;
+        marginRecord = (
+          left: localMargin.left,
+          top: localMargin.top,
+          right: localMargin.right,
+          bottom: localMargin.bottom
+        );
+      case == EdgeInsetsDirectional when textDirection == TextDirection.ltr:
+        final EdgeInsetsDirectional localMargin =
+            margin! as EdgeInsetsDirectional;
+        marginRecord = (
+          left: localMargin.start,
+          top: localMargin.top,
+          right: localMargin.end,
+          bottom: localMargin.bottom,
+        );
+      case == EdgeInsetsDirectional when textDirection == TextDirection.rtl:
+        final EdgeInsetsDirectional localMargin =
+            margin! as EdgeInsetsDirectional;
+        marginRecord = (
+          left: localMargin.end,
+          top: localMargin.top,
+          right: localMargin.start,
+          bottom: localMargin.bottom,
+        );
+      default:
+        marginRecord = (
+          left: 0,
+          top: 0,
+          right: 0,
+          bottom: 0,
+        );
+    }
+
     final Rect borderRect = Rect.fromLTWH(
-      0 + thickness / 2,
-      0 + thickness / 2,
-      size.width - thickness,
-      size.height - thickness,
+      0 + thickness / 2 + marginRecord.left,
+      0 + thickness / 2 + marginRecord.top,
+      size.width - thickness - marginRecord.left - marginRecord.right,
+      size.height - thickness - marginRecord.top - marginRecord.bottom,
     );
 
     final RRect rrect = RRect.fromRectAndRadius(
